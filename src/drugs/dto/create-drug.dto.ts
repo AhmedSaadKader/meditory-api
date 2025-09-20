f5e7d66dd5ab48f9d645bb756b272da6e191d62e
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2025-09-20 19:17:53 +0300

    swagger

--- src/drugs/dto/create-drug.dto.ts
+++ src/drugs/dto/create-drug.dto.ts
@@ -1,1 +1,93 @@
-export class CreateDrugDto {}
+import {
+  IsString,
+  IsOptional,
+  IsNumber,
+  IsPositive,
+  Min,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateDrugDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  original_id?: number;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  oldprice?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  price?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  active_raw?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  img?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  company?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  dosage_form?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  units?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  barcode?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  route?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  pharmacology?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  sold_times?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  date_updated?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  cosmo?: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Drugs API')
+    .setDescription('The Drugs API description')
+    .setVersion('1.0')
+    // .addTag('drugs')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
