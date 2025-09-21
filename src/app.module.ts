9ef27275d0c4a845bd9be1bed32fc22ebcf97a73
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2025-09-21 18:14:52 +0300

    fixup! fixup! fixup! fixup! fixup! fixup! use configService to handle env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,22 +13,29 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('DB_TYPE') as 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: parseInt(configService.get<string>('DB_PORT') || '5432'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [Drug],
-        synchronize: configService.get<string>('DB_SYNCHRONIZE') === 'true',
-        ssl: configService.get<string>('DB_SSL') === 'true',
-        extra: {
-          ssl: {
-            rejectUnauthorized: false,
+      useFactory: (configService: ConfigService) => {
+        console.log('DB_HOST:', configService.get<string>('DB_HOST'));
+        console.log('DB_PORT:', configService.get<string>('DB_PORT'));
+        console.log('DB_USERNAME:', configService.get<string>('DB_USERNAME'));
+        console.log('DB_DATABASE:', configService.get<string>('DB_DATABASE'));
+
+        return {
+          type: 'postgres' as const,
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(configService.get<string>('DB_PORT') || '5432'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [Drug],
+          synchronize: configService.get<string>('DB_SYNCHRONIZE') === 'true',
+          ssl: configService.get<string>('DB_SSL') === 'true',
+          extra: {
+            ssl: {
+              rejectUnauthorized: false,
+            },
           },
-        },
-      }),
+        };
+      },
     }),
     DrugsModule,
   ],
